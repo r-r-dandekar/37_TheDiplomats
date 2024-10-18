37c57aa8cb3df384dcb3723f2ef06929de541ff7
ShambhuG028 <158308873+ShambhuG028@users.noreply.github.com>
2024-10-18 23:58:51 +0530

    Update menubar.py

--- app/ui/widgets/menubar.py
+++ app/ui/widgets/menubar.py
@@ -1,5 +1,6 @@
-''' app/ui/widgets/menubar.py '''
+# app/ui/widgets/menubar.py
 from PyQt6.QtWidgets import QMenuBar
+from PyQt6.QtGui import QAction
 
 
 class MenuBar(QMenuBar):
@@ -12,12 +13,15 @@ class MenuBar(QMenuBar):
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
-        file_menu = self.addMenu("File")
-        # edit_menu = self.addMenu("Edit")
-        # view_menu = self.addMenu("View")
-        # help_menu = self.addMenu("Help")
 
-        # Add actions to the menus
-        file_menu.addAction(self.parent().topbar.actions_call["Open"]) # type: ignore
-        file_menu.addAction(self.parent().topbar.actions_call["Save"]) # type: ignore
-        file_menu.addAction(self.parent().topbar.actions_call["Exit"]) # type: ignore
+        # Create File Menu
+        file_menu = self.addMenu("File")
+
+        # Create actions for the File Menu
+        open_action = QAction("Open", self)
+        open_action.triggered.connect(parent.open_folder)  # Connect to the open_folder method
+        file_menu.addAction(open_action)
+
+        
+
+        # You can also add additional menus if needed
